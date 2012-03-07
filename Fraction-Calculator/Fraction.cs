b89a8ae81f184df7b6c7369b0997f0eabf0cff48
Igor Kostenko <work.kerogi@gmail.com>
2012-03-07 16:18:44 +0200

    Add stub for Fraction class

---
+++ Fraction-Calculator/Fraction.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+public class Fraction
+{
+    private int _numerator = 1;
+    private int _denominator = 1;
+    private bool _negative = false;
+
+    public int Numerator
+    {
+        get { return _numerator; }
+        set { _numerator = value; }
+    }
+
+    public int Denominator
+    {
+        get { return _denominator; }
+        set {
+                if (value == 0) 
+                {
+                    throw new ArgumentOutOfRangeException("value", value.ToString(), "Value must be non-zero integer");
+                }
+                _denominator = value; 
+            }
+    }
+
+    public bool IsNegative
+    {
+        get { return _negative; }
+        set { _negative = value; }
+    }
+
+	public Fraction(int num, int den)
+	{
+        if(num
+        if (den == 0)
+        {
+            throw new ArgumentOutOfRangeException("value", value.ToString(), "Value mast be greater than 0");
+        }
+	}
+}
